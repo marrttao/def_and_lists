2b603785e31c48794cee3f18de79f5d1d8a73b3e
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 01:58:30 +0200

    completed task 5

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+def multiply(a, b):
+    if a > b:
+        a, b = b, a
+
+    result = a
+    for i in range(a, b+1):
+        result *= i
+    print(result)
 if __name__ == '__main__':
-    pass
+    _a = int(input())
+    _b = int(input())
+    multiply(_a, _b)
