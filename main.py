87143f7b7c6e49ac661b0840e2ce9e56f01b3d85
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 01:46:42 +0200

    completed task 4

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+def min(arr):
+    min_val = arr[0]
+    for i in arr:
+        if i < min_val:
+            min_val = i
+    print(min_val)
 if __name__ == '__main__':
-    pass
+    array = []
+    for i in range(5):
+        array.append(int(input("Enter a number: ")))
+    min(array)
