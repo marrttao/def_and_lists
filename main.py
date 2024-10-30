78af3dc1e6d9aae07231b7cfc621394ee2f0550a
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 01:36:26 +0200

    completed task 3

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+def square(size, symbol, type_of_square):
+    if type_of_square:
+        for i in range(size):
+            print(f"{symbol}  " * size)
+    else:
+        for i in range(size):
+            if i == 0 or i == size - 1:
+                print(f"{symbol}  " * size)
+            else:
+                print(f"{symbol}  " + "   " * (size - 2) + f"{symbol}  ")
+
+
 if __name__ == '__main__':
-    pass
+    _size = int(input("Enter the size: "))
+    _symbol = input("Enter the symbol: ")[0]
+    _type_of_square = input("True False: ").lower() == 'true'
+    square(_size, _symbol, _type_of_square)
