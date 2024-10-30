3a06e107da7d11d09825e427af7979524329d1c2
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 01:50:00 +0200

    completed task 1

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 def formated_text():
-    try:
         print('\033[37m"Don\'t compare yourself with anyone in this world...\n\033[31mif\033[0m you \033[31mdo so\033[0m, you are insulting yourself.\033[37m"\033[0m\nBill Gates')
-    except Exception as e:
-        print(f'Error: {e}')
 if __name__ == '__main__':
     formated_text()
