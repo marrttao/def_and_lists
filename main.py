44c405bc13559ace8c1936b4cd3e34057c8fb845
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 02:03:36 +0200

    completed task 6

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+def value(number):
+    value = 0
+    while number > 0:
+        number //= 10
+        value += 1
+    print(value)
+
 if __name__ == '__main__':
-    pass
+    _number = int(input())
+    value(_number)
